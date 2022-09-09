4a51042eaef56a5663a3c4832673f4329b57dd40
Bright Anua <futurecantellu@yahoo.com>
2022-09-09 16:39:18 -0700

    Added code to print alphabets and ommit letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e' || x == 'q')
+		{
+			x = x + 1;
+			putchar(x);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

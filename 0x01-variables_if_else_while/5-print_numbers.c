3afe45cf0bf0e9b6e4ebad529b9e49814d12944c
Bright Anua <futurecantellu@yahoo.com>
2022-09-09 17:14:15 -0700

    Added code to ptint all numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ /**
+  * main - Entry point
+  *
+  * Return: 0 for success
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+	return (0);
+}

ee3f99733e544b02b02ecdadfbe226d8ce1d977d
Bright Anua <futurecantellu@yahoo.com>
2022-09-09 18:45:22 -0700

    Added code to print hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - a programe that prints the values of hex in lower case
+ * All codes must be inside main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x % 10, '0');
+	}
+
+	for (x = 'a'; x <= f; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}

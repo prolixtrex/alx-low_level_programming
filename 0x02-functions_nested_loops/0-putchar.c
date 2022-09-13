43f2ceb388d07c979fe7948f0835457a078d3ed3
Bright Anua <futurecantellu@yahoo.com>
2022-09-13 08:38:15 -0700

    Added code to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - A program the prints _putchar
+ *
+ * Return:Always 0 Success
+ */
+
+int main(void)
+{
+	int i = 0;
+	char word[8] = "_putchar";
+
+	while (i < 8)
+	{
+		_putchar(word);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}

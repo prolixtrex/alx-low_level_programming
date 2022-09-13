67f61938f8a9fa1becbcb0ecf6fe8a8983f193cf
Bright Anua <futurecantellu@yahoo.com>
2022-09-13 15:39:37 -0700

    Added code to return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdli.h>
+#include "main.h"
+
+/**
+ * _abs - a function that returns the absolute value of the input
+ * @int: the value to check
+ * Return: return value
+ */
+
+int _abs(int)
+{
+	return (abs(int));
+}

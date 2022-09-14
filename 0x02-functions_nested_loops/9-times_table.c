0f215f9ec449cb056b07a4905b7c9e699e440458
Bright Anua <futurecantellu@yahoo.com>
2022-09-13 17:28:01 -0700

    Prints times table up to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - prints times table up to 9
+ * return: nothing
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	i = 1;
+	while (i <= 9)
+	{
+		j = 1;
+
+		while (j <= 9)
+		{
+			_putchar((i * j) + '0');
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+}

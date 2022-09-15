06e9089e11912bf68adbaf123bd85574c401264f
Bright Anua <futurecantellu@yahoo.com>
2022-09-15 09:20:49 -0700

    checks for upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is upper case
+ * @c: input parameter
+ * Return: 1 if upper, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	int res;
+
+	if (isupper(c) != 0)
+	{
+		res = 1;
+	} else
+	{
+		res = 0;
+	}
+
+	return (res);
+}

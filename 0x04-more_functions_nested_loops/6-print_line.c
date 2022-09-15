f4593bc1d50733a9d2c21c9f2bd7c653f9e19998
Bright Anua <futurecantellu@yahoo.com>
2022-09-15 12:01:07 -0700

    Prints a line across terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints line across  the terminal using _
+ * @n: number of _ used to print the line
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++
+		}
+
+		_putchar('\n');
+	}
+}

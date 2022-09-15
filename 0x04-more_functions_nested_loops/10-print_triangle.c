e5fc7e751de8ea7fd21ef0c1f35a9004c64953ef
Bright Anua <futurecantellu@yahoo.com>
2022-09-15 15:11:54 -0700

    prints in triangular pattern

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints in triangular pattern
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		i = 0;
+		while (i < size)
+		{
+			j = size - 1;
+			while (j >= 0)
+			{
+				_putchar(' ');
+				j--;
+			}
+
+			k = 1;
+			while (k <= size)
+			{
+				_putchar('#');
+				j++;
+			}
+
+			_putchar('\n');
+			i++;
+		}
+	}
+}

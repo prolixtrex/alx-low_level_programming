fc91f4383ba88601b261f6aa437014e26e65ca03
Bright Anua <futurecantellu@yahoo.com>
2022-09-15 09:46:20 -0700

    Prints numbers fro 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar((i % 10) + '0');
+	}
+
+	_putchar('\n');
+}

e799f3cc7f41c6865f61814d0b780942783167e0
Bright Anua <futurecantellu@yahoo.com>
2022-09-20 06:53:50 -0700

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,6 +10,19 @@
 
 void print_rev(char *s)
 {
-	char str[] = rev s;
-	puts(str);
+	char temp;
+	int i, j;
+
+	i = 0;
+	j = strlen(s) - 1;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+
+		i++;
+		j--;
+	}
 }

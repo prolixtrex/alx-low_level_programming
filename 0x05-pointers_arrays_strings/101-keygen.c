67208bdc88df65a678860334f745335ef5bc2a3c
Bright Anua <futurecantellu@yahoo.com>
2022-09-20 11:13:12 -0700

    Generates random valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for 101-crackme
+ * Return: 0
+ */
+
+int main(void)
+{
+	int win, pass;
+
+	srand(time(0));
+	win = 2772;
+
+	while (win >= 127)
+	{
+		pass = ((rand() % 126) + 1);
+		printf("%c", pass);
+		win -= pass;
+	}
+
+	printf("%c", win);
+	return (0);
+}

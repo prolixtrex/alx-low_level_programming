f573ba67c8f9e3aefa0f48406ac511f0daa23b9f
Bright Anua <futurecantellu@yahoo.com>
2022-09-20 10:02:25 -0700

    Prints second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints 2nd half of a character
+ * @str: pointer to input string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+	int len = strlen(str);
+
+	if  (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+	} else
+	{
+		n = len / 2;
+	}
+
+	for (i = n - 1; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}

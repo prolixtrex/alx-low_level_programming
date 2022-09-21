dd84231fbb4800ca83314cc51142e445e3bd2c54
Bright Anua <futurecantellu@yahoo.com>
2022-09-21 08:52:58 -0700

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalise each string
+ * @str: pointer to input string
+ * Return: return the capitalizeded word
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	int sep = " \t\n,;.!?\"(){}";
+
+	i = 1;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= ('a' - 'A');
+	}
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i - 1] == sep[j] && (str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}

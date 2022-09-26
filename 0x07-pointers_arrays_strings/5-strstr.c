abefe21330dd77201daadde0a3b6fb20cf71ac67
Bright Anua <futurecantellu@yahoo.com>
2022-09-26 14:39:38 -0700

    Search for substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i]; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; needle[j]; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (haystack);
+				break;
 			}
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (heystack + i);
 		}
 	}
 	return ('\0');

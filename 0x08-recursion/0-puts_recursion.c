8fd95a942ed9fcaad9d1d6e727d352b3b6d06056
Bright Anua <futurecantellu@yahoo.com>
2022-09-28 04:16:11 -0700

    Prints string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints string using recursion
+ * @s: pointer to string input
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(_put_recursion(s));
+}

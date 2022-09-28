4a10dab1ff846d048f4a41b5539599c21e2fb469
Bright Anua <futurecantellu@yahoo.com>
2022-09-28 05:32:42 -0700

    Print facotrial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,11 @@
 
 int factorial(int n)
 {
-	if (n < 0) 
+	if (n < 0)
 	{
 		return (-1);
 	}
 	if (n == 0)
-	{
 		return (0);
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 }

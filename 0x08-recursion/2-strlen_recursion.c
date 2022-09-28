3a114e721c929c2359ef3c9e616cf26ee91c1255
Bright Anua <futurecantellu@yahoo.com>
2022-09-28 04:55:09 -0700

    Prints length of input string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print length of input string
+ * @s: pointer to input string;
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (0);
+	++len;
+	_strlen_recursion(s);
+
+	return (len);
+}

42fcca0ba1999cd8b3c4b77783eb2ff6914a1bde
Bright Anua <futurecantellu@yahoo.com>
2022-09-28 05:59:23 -0700

    Prints result of x power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints x to the power of y
+ * @x: integer input
+ * @y: power of the integer
+ * Return: returns result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y = 0)
+		return 1;
+	x *= _pow_recursion(x, y - 1);
+	return x;
+}

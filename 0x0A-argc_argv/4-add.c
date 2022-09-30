7994d27da9d2974a6dced75f2352e64e0df78d8e
Bright Anua <futurecantellu@yahoo.com>
2022-09-30 09:13:50 -0700

    Add multiple numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,23 +12,17 @@ int main(int argc, char **argv)
 {
 	int i, sum = 0;
 
-	if (argc < 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (*argv[i] < '0' || *argv[i] > '9')
 		{
-			if (*argv[i] < '0' || *argv[i] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
+			printf("Error\n");
+			return (1);
 		}
-
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }

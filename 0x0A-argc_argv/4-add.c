7ca4c9f781e441315315c2cf9fde597e7c8973fb
Bright Anua <futurecantellu@yahoo.com>
2022-09-30 17:44:07 -0700

    Add array of numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,17 @@
 
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		for (j = 0, argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (*argv[i][j] < '0' || *argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}

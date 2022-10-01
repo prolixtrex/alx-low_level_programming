8c26a3327ad1d6046f5e02dccfa7ec8906ad4d52
Bright Anua <futurecantellu@yahoo.com>
2022-09-30 17:46:48 -0700

    Add array of numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0, argv[i][j]; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
 			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{

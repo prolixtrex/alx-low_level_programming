cf25cb8aad8f56471b7a63b2a836267da88102d4
Bright Anua <futurecantellu@yahoo.com>
2022-10-03 23:27:12 -0700

    Create arrays and dynamic memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = malloc(sizeOf(char) * size);
+	s = malloc(sizeof(char) * size);
 
 	if (s == NULL)
 		return (NULL);

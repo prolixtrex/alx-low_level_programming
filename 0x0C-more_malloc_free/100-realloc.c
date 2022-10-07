996a873ae73281d1acc32317158cb9f4594b9719
Bright Anua <futurecantellu@yahoo.com>
2022-10-06 21:23:21 -0700

    Reallocate memory using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,6 +28,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
+		return (p);
+	}
+
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
 		for (i = 0; i < old_size && i < new_size; i++)
 			*((char *)p + 1) = *((char *)ptr + 1);
 		free(ptr);

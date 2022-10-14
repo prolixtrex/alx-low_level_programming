2cad6142006748aaa19e1f91cda3f9c2961114a7
Bright Anua <futurecantellu@yahoo.com>
2022-10-13 20:23:38 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(stings, char *);
+		str = va_arg(strings, char *);
 		if (separator != NULL && i > 0)
 			printf("%s", separator);
 		if (str == NULL)

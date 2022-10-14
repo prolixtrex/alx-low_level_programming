6112ad6a69c24afe17bc77c7114c29772c7974df
Bright Anua <futurecantellu@yahoo.com>
2022-10-13 19:24:05 -0700

    Print all number and separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: number separator
+ * @n: input values
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int n;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(nums, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(nums);
+	_putchar('\n');
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,5 +3,17 @@
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int _putchar(char c);
+
+/**
+ *
+ */
+
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 
 #endif
